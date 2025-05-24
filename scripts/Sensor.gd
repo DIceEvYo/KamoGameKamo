ae67dc6d7d1d62a2d2c3a2761243d09c6e0a8803
DIceEvYo <dpaulprojects@gmail.com>
2025-05-24 11:33:23 -0700

    すばらしい更新
    かも

--- scripts/Sensor.gd
+++ scripts/Sensor.gd
@@ -6,10 +6,30 @@ extends Sprite2D
 var falling_key_queue = []
 
 func _process(delta):
+	if falling_key_queue.size() > 0:
+		if falling_key_queue.front().has_passed:
+			falling_key_queue.pop_front()
+			#print("popped")
+	
 	if Input.is_action_just_pressed(key_name):
-		CreateFallingKey()
+		var key_to_pop = falling_key_queue.pop_front()
+		var distance_from_pass = abs(key_to_pop.pass_threshold - key_to_pop.global_position.y)
+		print(distance_from_pass)
+		key_to_pop.queue_free()
+		
+	#11:05 rhythm game tut
+	
+
 
 func CreateFallingKey():
 	var falling_key_scene = falling_key.instantiate()
 	get_tree().get_root().call_deferred("add_child", falling_key_scene)
 	falling_key_scene.Setup(position.x, key_name)
+	
+	falling_key_queue.push_back(falling_key_scene)
+
+
+func _on_spawn_timer_timeout() -> void:
+	CreateFallingKey()
+	$SpawnTimer.wait_time = randf_range(0.4,3)
+	$SpawnTimer.start()
